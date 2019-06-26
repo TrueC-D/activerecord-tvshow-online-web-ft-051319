df00968d73a0feef996636494a085a8c7e7bfd55
Alethea Cary <aletheacary@gmail.com>
2019-06-26 16:02:40 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,18 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+  end
+  def self.most_popular_show
+  end
+  def self.lowest_rating
+  end
+  def self.least_popular_show
+  end
+  def self.ratings_sum
+  end
+  def self.popular_shows
+  end
+  def self.shows_by_alphabetical_order
+    self.where()
+  end
+end
